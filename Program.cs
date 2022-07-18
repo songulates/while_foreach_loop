3f83a0419b9e34c8ed0a289e86170412a2b8dd66
songulates <songl.ates.02@gmail.com>
2022-07-18 14:48:52 +0300

    while commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace console_programlama
+{ 
+    class Program
+    {
+        static void Main(string[] args)//
+        {
+            Console.WriteLine("bir sayi girin");
+            int sayi=int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while(sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+            //1 den başlayarak konsoldan girilen sayı kadar ortalama hesaplayan console yazdırın
+            //a dan z ye kadar tüm harfleri yazdır
+            char karakter='a';
+            while(karakter<='z')
+            {
+                Console.WriteLine(karakter);
+                karakter++;
+            }
+
+            string[] cars={"ford","V","BMW"};
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+        }
+    }
+}
